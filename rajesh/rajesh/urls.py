d193de796d10242d89c44b637317439780f290a8
Manikandan-ven <nithyavenky18@gmail.com>
2024-12-08 06:21:55 +0530

    success

---
+++ rajesh/rajesh/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin 
+from django.urls import path 
+from muni import views 
+urlpatterns = [ 
+    path('admin/', admin.site.urls), 
+    path('powerlamp/',views.powerlamp,name="powerlamp"),
+    path('',views.powerlamp,name="powerlamproot")
+]
